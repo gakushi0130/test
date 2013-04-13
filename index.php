59856616d18f6ab55522ea9fa7c19d7951884c6c
gakushi0130 <gakushi0130@gmail.com>
2013-04-13 20:09:29 +0900

    testdb

--- index.php
+++ index.php
@@ -7,3 +7,22 @@
  *
  */
 echo phpinfo();
+
+
+
+$dbhost = 'testdb.ct8kfjivc4hn.ap-northeast-1.rds.amazonaws.com';
+$username = 'dbuser';
+$password = 'adgj6789';
+$dbname = 'testdb';
+
+$link = mysql_connect($dbhost, $username, $password, $dbname);
+mysql_select_db($dbname);
+
+$result = mysql_query("SELECT * FROM mytable");
+
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    printf ("ID: %s  Name: %s", $row["test_id"], $row["test_name"]);
+}
+
+mysql_free_result($result);
+
