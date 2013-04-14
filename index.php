57a6d3ba07249b9a74516f5193e2abf3da09ea31
gakushi0130 <gakushi0130@gmail.com>
2013-04-14 19:21:58 +0900

    test memcached2

--- index.php
+++ index.php
@@ -7,26 +7,44 @@
  *
  */
 
-echo "test2";
+echo "test3";
 
-# set specific endpoint for auto discovery
+/**
+ * Sample PHP code to show how to integrate with the Amazon ElastiCcache
+ * Auto Discovery feature.
+ */
+
+/* Configuration endpoint to use to initialize memcached client. This is only an example. */
 $server_endpoint = "test-gakushi.32ftn4.cfg.apne1.cache.amazonaws.com";
+/* Port for connecting to the ElastiCache cluster. This is only an example */
 $server_port = 11211;
 
-# connect and store record
-$stime = time();
+/**
+ * The following will initialize a Memcached client to utilize the Auto Discovery feature.
+ *
+ * By configuring the client with the Dynamic client mode with single endpoint, the
+ * client will periodically use the configuration endpoint to retrieve the current cache
+ * cluster configuration. This allows scaling the cache cluster up or down in number of nodes
+ * without requiring any changes to the PHP application.
+ */
+
 $dynamic_client = new Memcached();
 $dynamic_client->setOption(Memcached::OPT_CLIENT_MODE, Memcached::DYNAMIC_CLIENT_MODE);
 $dynamic_client->addServer($server_endpoint, $server_port);
-  $dynamic_client->set('key1', 'value', 600);
-  $dynamic_client->set('key2', 'value', 600);
-  $dynamic_client->set('key3', 'value', 600);
-  $dynamic_client->set('key4', 'value', 600);
-  $dynamic_client->set('key5', 'value', 600);
-  $dynamic_client->set('key6', 'value', 600);
-$ptime = time() - $stime;
-echo $ptime;
+$dynamic_client->set('key', 'value', 60);  // Store the data for 60 seconds in the cluster, the client will decide which node to store
 
-# verify serverlist
-$sl = $dynamic_client->getServerList();
-print_r($sl);
+$var = $dynamic_client->get('key');
+
+var_dump($var);
+
+
+/**
+ * Configuring the client with Static client mode disables the usage of Auto Discovery
+ * and the client operates as it did before the introduction of Auto Discovery. The user
+ * can then add a list of server endpoints.
+ */
+
+$static_client = new Memcached();
+$static_client->setOption(Memcached::OPT_CLIENT_MODE, Memcached::STATIC_CLIENT_MODE);
+$static_client->addServer($server_endpoint, $server_port);
+$static_client->set('key', 'value');  // Store the data in the cluster without expiration
